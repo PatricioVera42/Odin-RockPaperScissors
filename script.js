86f56ae0f62729c149d49fffdec369bcd7d684a6
patovera <verapatricio9@gmail.com>
2024-04-27 23:46:54 -0300

    all

--- script.js
+++ script.js
@@ -7,14 +7,44 @@ function getComputerChoice(){
     } else {
         return "scissors";
     }
-};
+}
 
 function getHumanChoice(){
-    let choice = window.prompt("Choose between rock, paper or scissors");
+    let choice = window.prompt("Choose between rock, paper or scissors").toLowerCase();
     while (choice != "rock" && choice != "paper" && choice != "scissors"){
-    choice = window.prompt("Choose between rock, paper or scissors");
+    choice = window.prompt("Choose between rock, paper or scissors").toLowerCase();
     }
     return choice;
 }
-let choice = getHumanChoice();
-console.log(choice);
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+    function playRound(humanChoice, computerChoice){
+        if (humanChoice == computerChoice){
+            console.log("It's a draw, you both choose:",humanChoice);
+        } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")){
+            humanScore++;
+            console.log("You win!",humanChoice,"beats",computerChoice);
+        } else {
+            computerScore++;
+            console.log("You lost!",computerChoice,"beats",humanChoice);
+        }
+    }
+    for (let i = 1; i <= 5; i++){
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore){
+        console.log("You won!", humanScore, "against", computerScore);
+    } else if (computerScore > humanScore){
+        console.log("You lost!", computerScore, "against", humanScore);
+    } else{
+        console.log("Its a draw!");
+    }
+}
+
+playGame();
